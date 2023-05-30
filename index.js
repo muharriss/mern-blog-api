6ae8d97de2f4ddf35b9140508f60776071d52474
muharriss <muhharris@gmail.com>
2023-05-30 07:04:05 +0700

    edit cors

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     res.setHeader("Access-Control-Allow-Credentials", "true")
+    res.setHeader("Access-Control-Max-Age", "1800");
     next();
 });
 
