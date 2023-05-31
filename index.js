53e8ac42823a88a8d9e9eea608540d916c4a0f0e
muharriss <muhharris@gmail.com>
2023-06-01 05:11:22 +0700

    edit cors

--- index.js
+++ index.js
@@ -28,16 +28,18 @@ const fileFilter = (req, file, cb) => {
         cb(null, false)
     }
 }
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*, http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    res.setHeader("Access-Control-Allow-Credentials", "true")
-    res.setHeader("Access-Control-Max-Age", "1800");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+//     next();
+// });
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'PUT'], // Izinkan metode HTTP tertentu
+    allowedHeaders: ['Content-Type', 'Authorization'] // Izinkan header tertentu
+  }));
 
 app.use(bodyParser.json()) // type JSON
 app.use('/images', express.static(path.join(__dirname, 'images')))
